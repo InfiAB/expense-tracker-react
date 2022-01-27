ade0bd9dd9319181c26043eab1e410e5c53b2789
InfiAB <tmtg360@gmail.com>
2022-01-28 02:33:36 +0530

    basic UI down

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { Header } from './components/Header'
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TansactionList } from './components/TansactionList';
+import { AddTansaction } from './components/AddTansaction';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+       
+      <Header />
+
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TansactionList />
+        <AddTansaction />
+      </div>
     </div>
   );
 }
