661208e5809a1bf60abdf4dbd3cd73a7e022a6b4
InfiAB <tmtg360@gmail.com>
2022-01-30 21:25:57 +0530

    db authentication/connection done.

--- config/db.js
+++ config/db.js
@@ -4,13 +4,12 @@ const connectDB = async() => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useCreateIndex: true,
             useUnifiedTopology: true
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
     } catch (err) {
-        console.log(`Error ${err.messafe}`.red);
+        console.log(`Error ${err.message}`.red);
         process.exit(1);
     }
 }
--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,21 +1,37 @@
+const Transaction = require('../models/Transaction')
+
 // @desc    Get all transactions
 // @route   GET /api/v1/transactions
 // @access  Public
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+    try {
+        console.log('Here!!');
+        const transactions = await Transaction.find();
+        console.log('But not here.')
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        })
+    }
 }
 
 
 // @desc    Add transactions
 // @route   POST /api/v1/transactions
 // @access  Public
-exports.addTransactions = (req, res, next) => {
+exports.addTransactions = async (req, res, next) => {
     res.send('POST transactions');
 }
 
 // @desc    Delete transactions
 // @route   GET /api/v1/transactions:id
 // @access  Public
-exports.deletetTransactions = (req, res, next) => {
+exports.deletetTransactions = async (req, res, next) => {
     res.send('DELETE transactions');
 }
